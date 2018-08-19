87759674bd050dc60a30abec8212b0f7a350b791
arturo.sanchez@mobileappscompany.us <asc_stb@hotmail.com>
2018-08-19 14:29:11 -0400

    Agregar archivos de proyecto.

---
+++ WellstarAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WellstarAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WellstarAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WellstarAPI.WebApiApplication" Language="C#" %>
